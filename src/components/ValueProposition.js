d5eddd0ff6cdf05277d1040b6ed2eb1b73105ee7
[Joana Moura] <[joanaa.mmoura@gmail.com]>
2020-10-15 15:48:51 +0100

    Edited CSS for ValuProposition Component

--- src/components/ValueProposition.js
+++ src/components/ValueProposition.js
@@ -1,40 +1,37 @@
-import React from "react";
-import "./ValueProposition.css";
+import React from 'react';
+import './ValueProposition.css';
 
 function ValueProposition() {
   return (
     <div className="valueProposition">
-      <div className="valueProposition__row valueProposition__titles">
-        <h3>Simplifica</h3>
-        <h3>Organiza</h3>
-        <h3>Poupa</h3>
-        <h3>Experiencia</h3>
-      </div>
-      <div className="valueProposition__row">
-        <img src={require("../resources/images/phone.svg")} alt="smartphone" />
-        <img
-          src={require("../resources/images/calendar.svg")}
-          alt="calendário"
-        />
-        <img src={require("../resources/images/money.svg")} alt="dinheiro" />
-        <img src={require("../resources/images/rocket.svg")} alt="universo" />
-      </div>
-      <div className="valueProposition__row">
-        <p>
-          Todas as actividades estão aqui, é só filtrar por idade, data, local e
-          voilá!
-        </p>
-        <p>
-          Os melhores horários para conciliar com o vosso calendário de família
-        </p>
-        <p>Nós somos flexíveis, pode pausar e cancelar mensalmente</p>
-        <p>
-          Experimentem as actividades que quiserem, um mundo de magia está à
-          vossa espera!
-        </p>
-      </div>
-      <div className="valueProposition__row"></div>
-      <div className="valueProposition__row"></div>
+      <div />
+      <img src={require('../resources/images/phone.svg')} alt="smartphone" />
+      <img src={require('../resources/images/calendar.svg')} alt="calendário" />
+      <img src={require('../resources/images/money.svg')} alt="dinheiro" />
+      <img src={require('../resources/images/rocket.svg')} alt="universo" />
+      <div />
+      <div />
+      <h3>Simplifica</h3>
+      <h3>Organiza</h3>
+      <h3>Poupa</h3>
+      <h3>Experiencia</h3>
+      <div />
+      <div />
+      <p>
+        Todas as actividades estão aqui, é só filtrar por idade, data ou local
+      </p>
+      <p>
+        Escolhe os melhores horários para conciliar com o calendário da vossa
+        família
+      </p>
+      <p>
+        Nós somos flexíveis, podes pausar e cancelar mensalmente, se precisares
+      </p>
+      <p>
+        Experimentem as actividades que quiserem, um mundo de magia está à vossa
+        espera!
+      </p>
+      <div />
     </div>
   );
 }
